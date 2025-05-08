b9039ec8fcb4c2d0ff665a38d6beef232b48be4e
Brianna Gonzalez <b3gonzal@uwaterloo.ca>
2025-05-08 17:02:40 -0400

    Create mockStore.js

---
+++ Routes/src/datastores/mockStore.js
@@ -0,0 +1,10 @@
+export async function fetchGreeting() {
+    // Simulated DB/Redis/etc access
+    return 'Hello from your service, via controller and route!';
+}
+
+export async function fetchGoodNight() {
+    // Simulated DB/Redis/etc access
+    return 'Good Night, World! Sweet dreams!';
+}
+  
